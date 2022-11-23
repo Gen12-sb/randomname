02560697215256c5fef2238a612ad353df3e69d6
Gen12-sb <118854026+Gen12-sb@users.noreply.github.com>
2022-11-23 21:52:46 +0300

    Final

--- main.py
+++ main.py
@@ -2,3 +2,36 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor, QPolygon
 from random import randint
+from PyQt5 import uic
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.a = False
+        self.pushButton.clicked.connect(self.start)
+        self.pushButton.clicked.connect(self.update)
+
+    def drawing(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        a = randint(1, 300)
+        b = randint(1, 800 - a // 2)
+        c = randint(1, 600 - a // 2)
+        qp.drawEllipse(b, c, a, a)
+
+    def paintEvent(self, event):
+        if self.a:
+            qp = QPainter()
+            qp.begin(self)
+            self.drawing(qp)
+            qp.end()
+
+    def start(self):
+        self.a = True
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
+
